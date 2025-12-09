9e2e5b36d9568f5a088cfa697a5ca4f4ecad549a
Daniel Eisenhardt <eisenhardtdaniel@protonmail.com>
2025-12-09 18:28:37 +0100

    CH1L5 Fileservers: it servers from the root directory

--- main.go
+++ main.go
@@ -10,9 +10,11 @@ func main() {
 
 	mux := http.NewServeMux()
 	server := &http.Server{
-		Addr: ":" + port,
+		Addr:    ":" + port,
 		Handler: mux,
 	}
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
