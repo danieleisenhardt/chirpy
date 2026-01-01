008b9e27150f64b44710e5b4db9a78fa1bfe7cff
Daniel Eisenhardt <eisenhardtdaniel@protonmail.com>
2026-01-01 09:28:51 +0100

    CH3L1 moved non-website endpoints under an /api namespace

--- main.go
+++ main.go
@@ -29,19 +29,19 @@ func main() {
 
 	apiCfg := &apiConfig{}
 
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("OK"))
 	})
 
-	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/metrics", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("Hits: " + fmt.Sprint(apiCfg.fileserverHits.Load())))
 	})
 
-	mux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("POST /api/reset", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		apiCfg.fileserverHits.Store(0)
